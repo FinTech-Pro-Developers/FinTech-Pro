a8fc92fd06cb460c16464ea1cb85e8ac07faa490
Kom02 <kom0211@gmail.com>
2025-04-09 17:15:53 -0600

    Refactor and update of libraries

--- fin_tech-pro/src/app/page.tsx
+++ fin_tech-pro/src/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
+import Header from "@/components/organisms/header";
+import Hero from "@/components/molecules/hero";
 export default function Page() {
   return (
     <div>
