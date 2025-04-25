c460d134920a8911228d62c28182114a4df9f835
Luis Carrión <louiss862@gmail.com>
2025-04-25 15:53:36 -0300

    feat(web): add semantics to the section

--- fin_tech-pro/src/components/molecules/hero.tsx
+++ fin_tech-pro/src/components/molecules/hero.tsx
@@ -1,18 +1,25 @@
+import Image from 'next/image'
+import placeholderImage from './../../../public/images/placeholder-image.png'
+
 export default function Hero() {
   return (
-    <div className="min-h-screen flex items-center justify-between px-10">
-      <div className="max-w-xl">
-        <div className="mb-4">
-          <h1 className="text-5xl font-bold">
-            Manage Your Finances with confidence
-          </h1>
-          <p className="mt-4 text-gray-600">
-            FinTech Pro provides powerful tools to track, analyze and optimize
-            your financial health. Take control of your money today{" "}
-          </p>
+    <section className="flex items-center justify-between py-[192px] px-4">
+      <div className="mx-auto flex max-md:flex-col items-center gap-1">
+        <div className="max-w-xl ">
+          <div className="mb-4 max-md:text-center">
+            <h1 className="text-5xl font-bold ">
+              Manage Your Finances with confidence
+            </h1>
+            <p className="mt-4 text-gray-600">
+              FinTech Pro provides powerful tools to track, analyze and optimize
+              your financial health. Take control of your money today{' '}
+            </p>
+          </div>
+        </div>
+        <div>
+          <Image src={placeholderImage} alt="image" />
         </div>
       </div>
-      <div>Image Placeholder</div>
-    </div>
-  );
+    </section>
+  )
 }
