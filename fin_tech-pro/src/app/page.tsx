fe83f628f3e4f41542be6d4a320a012b0551db67
Luis Carrión <louiss862@gmail.com>
2025-04-30 00:01:43 -0300

    feat(web): add featura component a to page

--- fin_tech-pro/src/app/page.tsx
+++ fin_tech-pro/src/app/page.tsx
@@ -1,8 +1,11 @@
 import Hero from '@/components/molecules/hero'
+import { Features } from '@/components/organisms/features'
+
 export default function Page() {
   return (
     <>
       <Hero />
+      <Features />
     </>
   )
 }
